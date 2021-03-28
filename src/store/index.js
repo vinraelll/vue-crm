7e07f102d9cab9f8deeadf8619c5958fa393b0db
vinraelll <53097647+vinraelll@users.noreply.github.com>
2021-03-28 05:13:32 +0200

    category validations

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
+import info from './info'
+import category from './category'
 
 Vue.use(Vuex)
 
@@ -16,10 +18,19 @@ export default new Vuex.Store({
       state.error = null
     }
   },
+  actions: {
+    async fetchCurrency() {
+      const key = process.env.VUE_APP_FIXER
+      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,UAH`)
+      return await res.json()
+    }
+  },
   getters: {
     error: s => s.error
   },
   modules: {
-    auth
+    auth,
+    info,
+    category
   }
 })
